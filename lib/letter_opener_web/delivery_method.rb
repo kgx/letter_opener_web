6702a9dd29b10271bf492e6d95f90f69f18306da
Keith Gaputis <keith@forwardlight.com>
2017-10-18 13:00:25 -0400

    add notifications into v1.3.0

--- lib/letter_opener_web/delivery_method.rb
+++ lib/letter_opener_web/delivery_method.rb
@@ -3,9 +3,17 @@ require 'letter_opener/delivery_method'
 module LetterOpenerWeb
   class DeliveryMethod < LetterOpener::DeliveryMethod
     def deliver!(mail)
+      begin
+        if ('EnvironmentUtils'.constantize rescue nil)
+          EnvironmentUtils.system_notification(mail.from.first.to_s || 'No Sender', mail.subject.to_s || 'No Subject')
+        end
+      rescue => error
+        if ('LoggingUtils'.constantize rescue nil)
+          LoggingUtils.write_to_log(error)
+        end
+      end
       location = File.join(settings[:location], "#{Time.now.to_i}_#{Digest::SHA1.hexdigest(mail.encoded)[0..6]}")
       messages = LetterOpener::Message.rendered_messages(location, mail)
-      # Launchy.open(URI.parse(URI.escape(messages.first.filepath)))
     end
   end
 end
